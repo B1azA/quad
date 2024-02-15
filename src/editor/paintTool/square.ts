fc0f3d6249dfc79e91dcf6a3d13debd606734179
Josef Blažek <blaza004@gmail.com>
2024-02-15 08:38:12 +0100

    square steps fix

--- src/editor/paintTool/square.ts
+++ src/editor/paintTool/square.ts
@@ -64,80 +64,72 @@ export class Square implements PaintTool {
 
         let radius = Math.round(Math.sqrt((center.x - a.x) ** 2 + (center.y - a.y) ** 2));
 
-        for (let x = center.x - radius; x <= center.x + radius; x++) {
-            let point1 = { x, y: center.y - radius };
+        if (center.x == a.x && center.y == a.y) {
+            let pixelColor = image.getPixel(a);
 
-            if (point1.x < size.width && point1.x >= 0 && point1.y < size.height && point1.y >= 0) {
-                let pixelColor = image.getPixel(point1);
-
-                if (!layer.isTemplate()) {
-                    let paintMinistep = new PaintMiniStep(point1, pixelColor);
-                    this.step?.addMiniStep(paintMinistep)
-                }
-
-                image.putPixel(point1, color);
+            if (!layer.isTemplate()) {
+                let paintMinistep = new PaintMiniStep(a, pixelColor);
+                this.step?.addMiniStep(paintMinistep)
             }
 
-            let point2 = { x, y: center.y + radius };
+            image.putPixel(a, color);
+        } else {
+            for (let x = center.x - radius + 1; x <= center.x + radius - 1; x++) {
+                let point1 = { x, y: center.y - radius };
 
-            if (point2.x < size.width && point2.x >= 0 && point2.y < size.height && point2.y >= 0) {
-                let pixelColor = image.getPixel(point2);
+                if (point1.x < size.width && point1.x >= 0 && point1.y < size.height && point1.y >= 0) {
+                    let pixelColor = image.getPixel(point1);
 
-                if (!layer.isTemplate()) {
-                    let paintMinistep = new PaintMiniStep(point2, pixelColor);
-                    this.step?.addMiniStep(paintMinistep)
+                    if (!layer.isTemplate()) {
+                        let paintMinistep = new PaintMiniStep(point1, pixelColor);
+                        this.step?.addMiniStep(paintMinistep)
+                    }
+
+                    image.putPixel(point1, color);
                 }
 
-                image.putPixel(point2, color);
+                let point2 = { x, y: center.y + radius };
+
+                if (point2.x < size.width && point2.x >= 0 && point2.y < size.height && point2.y >= 0) {
+                    let pixelColor = image.getPixel(point2);
+
+                    if (!layer.isTemplate()) {
+                        let paintMinistep = new PaintMiniStep(point2, pixelColor);
+                        this.step?.addMiniStep(paintMinistep)
+                    }
+
+                    image.putPixel(point2, color);
+                }
+            }
+
+            for (let y = center.y - radius; y <= center.y + radius; y++) {
+                let point1 = { x: center.x - radius, y };
+
+                if (point1.x < size.width && point1.x >= 0 && point1.y < size.height && point1.y >= 0) {
+                    let pixelColor = image.getPixel(point1);
+
+                    if (!layer.isTemplate()) {
+                        let paintMinistep = new PaintMiniStep(point1, pixelColor);
+                        this.step?.addMiniStep(paintMinistep)
+                    }
+
+                    image.putPixel(point1, color);
+                }
+
+                let point2 = { x: center.x + radius, y };
+
+                if (point2.x < size.width && point2.x >= 0 && point2.y < size.height && point2.y >= 0) {
+                    let pixelColor = image.getPixel(point2);
+
+                    if (!layer.isTemplate()) {
+                        let paintMinistep = new PaintMiniStep(point2, pixelColor);
+                        this.step?.addMiniStep(paintMinistep)
+                    }
+
+                    image.putPixel(point2, color);
+                }
             }
         }
-
-        for (let y = center.y - radius; y <= center.y + radius; y++) {
-            let point1 = { x: center.x - radius, y };
-
-            if (point1.x < size.width && point1.x >= 0 && point1.y < size.height && point1.y >= 0) {
-                let pixelColor = image.getPixel(point1);
-
-                if (!layer.isTemplate()) {
-                    let paintMinistep = new PaintMiniStep(point1, pixelColor);
-                    this.step?.addMiniStep(paintMinistep)
-                }
-
-                image.putPixel(point1, color);
-            }
-
-            let point2 = { x: center.x + radius, y };
-
-            if (point2.x < size.width && point2.x >= 0 && point2.y < size.height && point2.y >= 0) {
-                let pixelColor = image.getPixel(point2);
-
-                if (!layer.isTemplate()) {
-                    let paintMinistep = new PaintMiniStep(point2, pixelColor);
-                    this.step?.addMiniStep(paintMinistep)
-                }
-
-                image.putPixel(point2, color);
-            }
-        }
-
-
-
-        // for (let x = center.x - radius; x <= center.x + radius; x++) {
-        //     for (let y = center.y - radius; y <= center.y + radius; y++) {
-        //         let point = { x, y };
-        //
-        //         if (point.x < size.width && point.x >= 0 && point.y < size.height && point.y >= 0) {
-        //             let pixelColor = image.getPixel(point);
-        //
-        //             if (!layer.isTemplate()) {
-        //                 let paintMinistep = new PaintMiniStep(point, pixelColor);
-        //                 this.step?.addMiniStep(paintMinistep)
-        //             }
-        //
-        //             image.putPixel(point, color);
-        //         }
-        //     }
-        // }
 
         layer.setImage(image);
     }
